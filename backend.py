006c11229f55812b0c22b85b1781dccb0399b9d6
M-Bahy <mohamedayman2942002@gmail.com>
2025-01-18 19:29:22 +0200

    slot mapping

--- backend.py
+++ backend.py
@@ -1,4 +1,23 @@
 import pickle
+from schedule import Schedule
+
+day_mapping = {
+    "Saturday": 0,
+    "Sunday": 1,
+    "Monday": 2,
+    "Tuesday": 3,
+    "Wednesday": 4,
+    "Thursday": 5,
+    "Friday": 6,
+}
+
+slot_mapping = {
+    "First Slot 8:15-9:45": 0,
+    "Second Slot 10:00-11:30": 1,
+    "Third Slot 11:45-1:15": 2,
+    "Fourth Slot 1:45-3:15": 3,
+    "Fifth Slot 3:30-5:00": 4,
+}
 
 
 def deserialize(name):
@@ -8,4 +27,12 @@ def deserialize(name):
 
 
 subjects = deserialize("all_subjects")
-print(subjects[0])
+print(subjects[40].day)  # Sunday
+print(subjects[40].slot)  # First Slot 8:15-9:45
+s = Schedule()
+print(s)
+mapped_day = day_mapping[subjects[40].day]
+mapped_slot = slot_mapping[subjects[40].slot]
+s.set_slot(mapped_day, mapped_slot, subjects[40].name)
+s.free()
+print(s)
