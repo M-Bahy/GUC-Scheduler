446c1b213cf724a186b98d868fbfa98110da184a
M-Bahy <mohamedayman2942002@gmail.com>
2025-01-18 15:32:17 +0200

    lgtm

--- manipulation.py
+++ manipulation.py
@@ -1,18 +1,21 @@
 import pandas as pd
 
-
 OG_PATH = "og.csv"
+OUTPUT_PATH = "output2.csv"
 BETWEEN_CELLS_SPLIT = "      "
 INNER_CELL_SPLIT = " "
 LECTURE = "Lecture"
 LAB = "Lab"
 TUTORIAL = "Tut"
 MAPPING = {"DMET1001": "Image Processing", "NETW1009": "Cloud Computing"}
+
 df = pd.read_csv(OG_PATH, index_col=0)
+output_df = pd.DataFrame(index=df.index, columns=df.columns)
 
 for i in range(0, len(df.index)):  # loop through rows
     for j in range(0, len(df.columns)):  # loop through columns
         content = df.iloc[i, j].split(BETWEEN_CELLS_SPLIT)
+        new_content = []
         for subject in content:
             # loop through the content of the cell (contains multiple subjects)
             subject = subject.split(INNER_CELL_SPLIT)
@@ -32,9 +35,14 @@ for i in range(0, len(df.index)):  # loop through rows
             # replace the code with its mapping if it exists
             if code in MAPPING:
                 code = MAPPING[code]
-            # put the code back in the subject
-            subject[-3] = code
-            subject.pop(-2)
-            subject = " ".join(subject)
+                # put the code back in the subject
+                subject[-3] = code
+                subject.pop(-2)
+                subject = " ".join(subject)
+                new_content.append(subject)
 
-            print(subject)
+        # Join subjects with a line break
+        output_df.iloc[i, j] = "\n".join(new_content)
+
+# Save the output DataFrame to a CSV file
+output_df.to_csv(OUTPUT_PATH)
