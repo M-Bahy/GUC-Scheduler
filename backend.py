0902b0d16874e391c0eff33e019d2aa3673ddfc7
M-Bahy <mohamedayman2942002@gmail.com>
2025-01-18 19:19:20 +0200

    deserialization works

---
+++ backend.py
@@ -0,0 +1,11 @@
+import pickle
+
+
+def deserialize(name):
+    name = f"pickles/{name}.pickle"
+    with open(name, "rb") as infile:
+        return pickle.load(infile)
+
+
+subjects = deserialize("all_subjects")
+print(subjects[0])
