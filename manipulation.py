8e864005a2e2b22707d953d26230ce9d9ea23f91
M-Bahy <mohamedayman2942002@gmail.com>
2025-01-18 15:08:49 +0200

    hope

--- manipulation.py
+++ manipulation.py
@@ -1,7 +1,28 @@
 import pandas as pd
 
+
 OG_PATH = "og.csv"
+BETWEEN_CELLS_SPLIT = "      "
+INNER_CELL_SPLIT = " "
+df = pd.read_csv(OG_PATH, index_col=0)
 
-df = pd.read_csv(OG_PATH)
+# Access the first data cell
+first_data_cell = df.iloc[1, 2].split(BETWEEN_CELLS_SPLIT)
+print("First data cell:", first_data_cell)
 
-print(df.columns[1:])
+# Print the column headers
+print("Column headers:", df.columns.tolist())
+
+# Print the row headers
+print("Row headers:", df.index.tolist())
+
+# loop through the data cells
+for i in range(0, len(df.index)):
+    for j in range(0, len(df.columns)):
+        content = df.iloc[i, j].split(BETWEEN_CELLS_SPLIT)
+        for subject in content:
+            subject = subject.split(INNER_CELL_SPLIT)
+            # remove empty strings
+            subject = list(filter(None, subject))
+            print("Row:", i, "Column:", j, "Subject:", subject)
+            break
