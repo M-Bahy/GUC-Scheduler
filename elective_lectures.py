7d6b2ca4110ef596c217004dc615d8fade3d1b2e
M-Bahy <mohamedayman2942002@gmail.com>
2025-01-25 14:10:40 +0200

    for me

--- elective_lectures.py
+++ elective_lectures.py
@@ -3,11 +3,14 @@ from data import deserialize, serialize, get_subject
 import os
 from data import DAY_MAPPING, SLOT_MAPPING, SEMINARS
 
-cloud_l1 = get_subject("NETW1009")
-cloud_l2 = get_subject("NETW1009", "L002")
-image_l1 = get_subject("DMET1001")
-image_l2 = get_subject("DMET1001", "L002")
-code = "CSEN1140"
+seminar_code = "CSEN1008"
+elect1_code = "NETW1009"  # Cloud Computing
+elect2_code = "DMET1067"  # Deep Learning
+
+cloud_l1 = get_subject(elect1_code)
+cloud_l2 = get_subject(elect1_code, "L002")
+dlcv_l1 = get_subject("DMET1067")
+# image_l2 = get_subject("DMET1001", "L002")
 
 
 def add_elective_lecture(seminar_code, lecture1, lecture2):
@@ -46,43 +49,43 @@ def add_elective_lecture(seminar_code, lecture1, lecture2):
 # cloud_l2 + image_l1
 # cloud_l2 + image_l2
 
-c1 = add_elective_lecture(code, cloud_l1, image_l1)
-c2 = add_elective_lecture(code, cloud_l1, image_l2)
-c3 = add_elective_lecture(code, cloud_l2, image_l1)
-c4 = add_elective_lecture(code, cloud_l2, image_l2)
+c1 = add_elective_lecture(seminar_code, cloud_l1, dlcv_l1)
+# c2 = add_elective_lecture(code, cloud_l1, image_l2)
+c3 = add_elective_lecture(seminar_code, cloud_l2, dlcv_l1)
+# c4 = add_elective_lecture(code, cloud_l2, image_l2)
 
 os.makedirs(
-    f"pickles/Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L001_DMET1001_L001",
+    f"pickles/Seminars with elective lectures/{elect1_code}_{elect2_code}/{elect1_code}_L001_{elect2_code}_L001",
     exist_ok=True,
 )
 os.makedirs(
-    f"pickles/Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L001_DMET1001_L002",
+    # f"pickles/Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L001_DMET1001_L002",
     exist_ok=True,
 )
 os.makedirs(
-    f"pickles/Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L002_DMET1001_L001",
+    f"pickles/Seminars with elective lectures/{elect1_code}_{elect2_code}/{elect1_code}_L002_{elect2_code}_L001",
     exist_ok=True,
 )
 os.makedirs(
-    f"pickles/Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L002_DMET1001_L002",
+    # f"pickles/Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L002_DMET1001_L002",
     exist_ok=True,
 )
 
 serialize(
     c1,
-    f"Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L001_DMET1001_L001/NETW1009_L001_DMET1001_L001",
+    f"Seminars with elective lectures/{elect1_code}_{elect2_code}/{elect1_code}_L001_{elect2_code}_L001/{elect1_code}_L001_{elect2_code}_L001",
 )
-serialize(
-    c2,
-    f"Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L001_DMET1001_L002/NETW1009_L001_DMET1001_L002",
-)
+# serialize(
+#     c2,
+#     f"Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L001_DMET1001_L002/NETW1009_L001_DMET1001_L002",
+# )
 serialize(
     c3,
-    f"Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L002_DMET1001_L001/NETW1009_L002_DMET1001_L001",
+    f"Seminars with elective lectures/{elect1_code}_{elect2_code}/{elect1_code}_L002_{elect2_code}_L001/{elect1_code}_L002_{elect2_code}_L001",
 )
-serialize(
-    c4,
-    f"Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L002_DMET1001_L002/NETW1009_L002_DMET1001_L002",
-)
+# serialize(
+#     c4,
+#     f"Seminars with elective lectures/NETW1009_DMET1001/NETW1009_L002_DMET1001_L002/NETW1009_L002_DMET1001_L002",
+# )
 
 print("Done")
